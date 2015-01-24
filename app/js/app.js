6cefbfa41e1d9bfd06713ad3dc008bff47da4e74
Joao Vicente <vicentejo@dnb.com>
2015-01-24 12:13:34 +0000

    Added introduction section

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,31 @@
-    var myapp = angular.module('myapp', ["ui.router", "myControllers", 'ui.map', 'ui.event'])
-    myapp.config(function($stateProvider, $urlRouterProvider){
-      
-      // For any unmatched url, send to /map
-      $urlRouterProvider.otherwise("/map")
-      
-      $stateProvider
-        .state('map', {
-            url: "/map",
-            templateUrl: "partials/map.html",
-            controller: "MapCtrl"
-        })
-          
-        .state('gallery', {
-            url: "/gallery",
-            templateUrl: "partials/gallery.html",
-            controller: function($scope, $http){
-            	$scope.title = "Gallery"
-		// Get geoArtworks
-		$http.get('data/geoArtworks.json').success(function(data) {
-		$scope.geoArtworks= data;
-			});
-            }
-        })
+var myapp = angular.module('myapp', ["ui.router", "myControllers", 'ui.map', 'ui.event'])
+myapp.config(function($stateProvider, $urlRouterProvider){
+
+    // For any unmatched url, send to /map
+    $urlRouterProvider.otherwise("/introduction")
+
+    $stateProvider
+    
+    .state('introduction', {
+        url: "/introduction",
+        templateUrl: "partials/introduction.html",
     })
+
+    .state('map', {
+        url: "/map",
+        templateUrl: "partials/map.html",
+        controller: "MapCtrl"
+    })
+
+    .state('gallery', {
+        url: "/gallery",
+        templateUrl: "partials/gallery.html",
+        controller: function($scope, $http){
+            $scope.title = "Gallery"
+            // Get geoArtworks
+            $http.get('data/geoArtworks.json').success(function(data) {
+                $scope.geoArtworks= data;
+            });
+        }
+    })
+})
